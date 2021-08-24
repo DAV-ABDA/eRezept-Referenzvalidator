a81b5ba0598cdc62ed340542678cac78f9c1bac3
Martin Weigel <m.weigel@abda.de>
2021-08-24 16:51:20 +0200

    Update hapi-fhir dependencies to 5.6.0-PRE2-SNAPSHOT (including update of fhir-core to 5.4.10)

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,13 +18,15 @@ repositories {
     }
 }
 
+val hapiFhirVersion = "5.6.0-PRE2-SNAPSHOT"
+
 dependencies {
     implementation("org.apache.logging.log4j:log4j-api:2.14.1")
     implementation("org.apache.logging.log4j:log4j-core:2.14.1")
     implementation("org.slf4j:slf4j-log4j12:1.7.31")
-    implementation("ca.uhn.hapi.fhir:hapi-fhir-validation:5.6.0-PRE1-SNAPSHOT")
-    implementation("ca.uhn.hapi.fhir:hapi-fhir-structures-r4:5.6.0-PRE1-SNAPSHOT")
-    implementation("ca.uhn.hapi.fhir:hapi-fhir-validation-resources-r4:5.6.0-PRE1-SNAPSHOT")
+    implementation("ca.uhn.hapi.fhir:hapi-fhir-validation:$hapiFhirVersion")
+    implementation("ca.uhn.hapi.fhir:hapi-fhir-structures-r4:$hapiFhirVersion")
+    implementation("ca.uhn.hapi.fhir:hapi-fhir-validation-resources-r4:$hapiFhirVersion")
 }
 
 group = "fhir.abda.de"

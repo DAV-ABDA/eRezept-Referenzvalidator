34b8920d3d66c0edd8b9d9fdd4cf77e03ac04289
Martin Weigel <m.weigel@abda.de>
2021-10-20 10:07:52 +0200

    Add helper class for resource parsing

---
+++ core/src/main/java/de/abda/fhir/validator/core/util/ParserHelper.java
@@ -0,0 +1,25 @@
+package de.abda.fhir.validator.core.util;
+
+import ca.uhn.fhir.context.FhirContext;
+import ca.uhn.fhir.parser.IParser;
+import org.hl7.fhir.instance.model.api.IBaseResource;
+
+public class ParserHelper {
+
+    private FhirContext ctx = null;
+
+    public ParserHelper(FhirContext ctx) {
+        this.ctx = ctx;
+    }
+
+    public ParserHelper() {
+        this.ctx = FhirContext.forR4();
+    }
+
+    public IBaseResource parseString(String xmlInput) {
+        IParser xmlParser = ctx.newXmlParser();
+        IBaseResource resource = xmlParser.parseResource(xmlInput);
+        return resource;
+    }
+
+}

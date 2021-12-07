2aae0eb5e6b99f7d39e9e247e36402373332e1e5
Martin Weigel <m.weigel@abda.de>
2021-12-07 10:21:19 +0100

    Add whitelisting for bad identifier in http://fhir.abda.de/eRezeptAbgabedaten/StructureDefinition/DAV-PR-Base-ZusatzdatenHerstellung

--- core/src/main/java/de/abda/fhir/validator/core/util/WhiteListHelper.java
+++ core/src/main/java/de/abda/fhir/validator/core/util/WhiteListHelper.java
@@ -9,18 +9,30 @@ public class WhiteListHelper {
 
     public static void applyWhiteLists(List<SingleValidationMessage> messages) {
         removeKBVIdentifierWarnings(messages);
+        removeDAVActorIdentifierErrors(messages);
         //add custom whitelist filters here
     }
 
     public static void removeKBVIdentifierWarnings(List<SingleValidationMessage> messages) {
         messages.removeIf(singleValidationMessage -> {
-            if(singleValidationMessage.getSeverity() == ResultSeverityEnum.WARNING &&
+            if(singleValidationMessage.getSeverity() == ResultSeverityEnum.WARNING && (
                     (singleValidationMessage.getMessage().contains("http://fhir.de/CodeSystem/identifier-type-de-basis#GKV")) ||
                     (singleValidationMessage.getMessage().contains("http://terminology.hl7.org/CodeSystem/v2-0203#LANR")) ||
                     (singleValidationMessage.getMessage().contains("http://terminology.hl7.org/CodeSystem/v2-0203#BSNR"))
-            ) {
-                return true;
-            }
+            )) return true;
+            else return false;
+        });
+    }
+
+    public static void removeDAVActorIdentifierErrors(List<SingleValidationMessage> messages) {
+        messages.removeIf(singleValidationMessage -> {
+            if (singleValidationMessage.getSeverity() == ResultSeverityEnum.ERROR &&
+                    (
+                        (singleValidationMessage.getMessage().contains("http://fhir.abda.de/Identifier/DAV-Herstellerschluessel") && singleValidationMessage.getMessage().contains("http://fhir.de/NamingSystem/arge-ik/iknr")) ||
+                        singleValidationMessage.getMessage().contains("http://fhir.abda.de/eRezeptAbgabedaten/StructureDefinition/DAV-PR-ERP-ZusatzdatenEinheit") ||
+                        singleValidationMessage.getMessage().contains("http://fhir.abda.de/eRezeptAbgabedaten/StructureDefinition/DAV-PR-Base-ZusatzdatenHerstellung")
+                    )
+            ) return true;
             else return false;
         });
     }

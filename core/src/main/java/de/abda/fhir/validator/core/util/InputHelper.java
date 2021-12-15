016cb19d8d283122c0e95d2556db0b828a9faf13
Martin Weigel <m.weigel@abda.de>
2021-12-15 10:58:07 +0100

    Fix version removal helper

--- core/src/main/java/de/abda/fhir/validator/core/util/InputHelper.java
+++ core/src/main/java/de/abda/fhir/validator/core/util/InputHelper.java
@@ -1,9 +1,9 @@
 package de.abda.fhir.validator.core.util;
 
-import java.util.regex.MatchResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -15,7 +15,7 @@ public class InputHelper {
         String patternString = "(['\"])([Hh][Tt][Tt][Pp][Ss]?://[^|'\"<>\\s#]+/StructureDefinition/[^|'\"<>\\s#]+)(\\|[^'\"<>\\s#]+)(['\"])"; // Identify canonical profile URLs
         Pattern pattern = Pattern.compile(patternString);
         Matcher matcher = pattern.matcher(validatorInput);
-        if (matcher.matches()) {
+        if (matcher.find()) {
             MatchResult matchResult = matcher.toMatchResult();
             String replacement = matchResult.group(1) + matchResult.group(2) + matchResult.group(4);
             validatorInput = matcher.replaceAll(replacement);

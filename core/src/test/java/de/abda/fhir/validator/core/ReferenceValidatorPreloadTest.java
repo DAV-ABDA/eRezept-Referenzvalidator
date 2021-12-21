17878e2bc53ffd6ec2a23164d886c2df31e6b213
Martin Weigel <m.weigel@abda.de>
2021-12-21 12:45:17 +0100

    Reenable preload test for TA7 SammelrechnungsBundle

--- core/src/test/java/de/abda/fhir/validator/core/ReferenceValidatorPreloadTest.java
+++ core/src/test/java/de/abda/fhir/validator/core/ReferenceValidatorPreloadTest.java
@@ -1,9 +1,8 @@
 package de.abda.fhir.validator.core;
 
-import static de.abda.fhir.validator.core.ProfileForPreloading.*;
-
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+
+import static de.abda.fhir.validator.core.ProfileForPreloading.*;
 
 /**
  * @author RupprechJo
@@ -15,10 +14,6 @@ public class ReferenceValidatorPreloadTest {
     ReferenceValidator validator = new ReferenceValidator();
     validator.preloadAllSupportedValidators(KBV_PR_ERP_BUNDLE);
     validator.preloadAllSupportedValidators(ERX_RECEIPT, DAV_PR_ERP_ABGABEDATEN_BUNDLE);
-  }
-  @Disabled // TODO: Fehlverhalten Endlosschleife bei Generierung TA7 SnapShot
-  @Test void testSammelrechnung(){
-    ReferenceValidator validator = new ReferenceValidator();
     validator.preloadAllSupportedValidators(GKVSV_PR_TA7_SAMMELRECHNUNG);
   }
 
